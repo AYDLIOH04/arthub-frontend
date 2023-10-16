4211eb9a22e55deea185d35de3354c1993a3b94d
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2023-10-16 13:25:46 +0500

    рефакторинг

--- src/components/header/header-links.ts
+++ src/components/header/header-links.ts
@@ -14,6 +14,6 @@ export const links: NavLink[] = [
 const linkStyles = "text-white text-[16px] rounded-[8px] p-2 duration-200 easy-in inline"
 const smallScreens = "sm:text-[24px] sm:p-3";
 
-export const navLinkStyles = `${linkStyles} hover:bg-indigo-500 max-w-[10 0px] ${smallScreens}`
+export const navLinkStyles = `${linkStyles} hover:bg-indigo-500 max-w-[100px] ${smallScreens}`
 export const authLinkStyles = `${linkStyles} hover:bg-white hover:text-indigo-400 cursor-pointer ${smallScreens}`
 export const activeNavLinkStyles = " bg-indigo-500";
