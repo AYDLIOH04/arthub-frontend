b2e4446327bd359fe9a9e2f0d552263768355b37
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2023-11-11 16:36:50 +0500

    убран лишний css (теперь все через tailwind)

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,13 +1,9 @@
 import Footer from "@/components/home/Footer";
 import StarsBackground from "@/components/home/stars-bg";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="home-layout">
+    <main className="w-full h-full sm:bg-[url('/home/bg-image.jpg')] bg-[url('/home/mobile-bg.png')] bg-cover bg-center bg-no-repeat overflow-hidden relative">
       <StarsBackground />
       {children}
       <Footer />
