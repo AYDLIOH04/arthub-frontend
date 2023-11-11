0f6e2f7eaa3acdff6eaad9d309bad6a4ea72bfcc
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2023-11-11 23:16:42 +0500

    изменены файлы заднего фона на более прозрачные

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import StarsBackground from "@/components/home/stars-bg";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="w-full h-full sm:bg-[url('/home/bg-image.jpg')] bg-[url('/home/mobile-bg.png')] bg-cover bg-center bg-no-repeat overflow-hidden relative">
+    <main className="w-full h-full sm:bg-[url('/home/bg-image.png')] bg-[url('/home/mobile-bg.png')] bg-cover bg-center bg-no-repeat overflow-hidden relative">
       <StarsBackground />
       {children}
       <Footer />
