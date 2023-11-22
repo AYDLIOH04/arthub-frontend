b3f6c64ed2287b71349f7e721e5eab6bf7f8093b
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2023-11-22 21:54:01 +0500

    фикс ошибки с локальными файлами

--- src/components/UI/icons.tsx
+++ src/components/UI/icons.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
 export const MacosIcon = () => (
-  <Image src="/icons/macos-icon.png" width={35} height={35} alt={""} />
+  <Image src="/icons/Macos-icon.png" width={35} height={35} alt={""} />
 );
 
