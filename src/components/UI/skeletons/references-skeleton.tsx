4f8532f40e63aa5aeba2ce810e996d71730ff7ab
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2023-12-26 20:46:29 +0500

    3 итерация (MVP)

--- src/components/UI/skeletons/references-skeleton.tsx
+++ src/components/UI/skeletons/references-skeleton.tsx
@@ -1,13 +1,7 @@
-const ReferenceItemSkeleton = ({
-  height,
-  smHeight,
-}: {
-  height: number;
-  smHeight: number;
-}) => {
+const ReferenceItemSkeleton = ({ height }: { height: number }) => {
   return (
     <div
-      className={`sm:h-[${height}px] h-[${smHeight}px] w-full bg-gradient-to-br from-main_purple via-second_purple to-main_purple animate-pulse`}
+      className={`sm:h-[${height}px] w-full bg-gradient-to-br from-main_purple via-second_purple to-main_purple animate-pulse`}
     />
   );
 };
@@ -16,19 +10,19 @@ const ReferencesSkeleton = () => {
   return (
     <div className="flex flex-row gap-2 mt-8">
       <div className="flex flex-col gap-2 sm:w-1/3 w-1/2">
-        <ReferenceItemSkeleton height={400} smHeight={200} />
-        <ReferenceItemSkeleton height={500} smHeight={300} />
-        <ReferenceItemSkeleton height={550} smHeight={350} />
+        <ReferenceItemSkeleton height={400} />
+        <ReferenceItemSkeleton height={500} />
+        <ReferenceItemSkeleton height={550} />
       </div>
       <div className="flex flex-col gap-2 sm:w-1/3 w-1/2">
-        <ReferenceItemSkeleton height={550} smHeight={350} />
-        <ReferenceItemSkeleton height={400} smHeight={200} />
-        <ReferenceItemSkeleton height={500} smHeight={300} />
+        <ReferenceItemSkeleton height={550} />
+        <ReferenceItemSkeleton height={400} />
+        <ReferenceItemSkeleton height={500} />
       </div>
       <div className="sm:flex flex-col gap-2 w-1/3 hidden">
-        <ReferenceItemSkeleton height={400} smHeight={200} />
-        <ReferenceItemSkeleton height={550} smHeight={350} />
-        <ReferenceItemSkeleton height={500} smHeight={300} />
+        <ReferenceItemSkeleton height={400} />
+        <ReferenceItemSkeleton height={550} />
+        <ReferenceItemSkeleton height={500} />
       </div>
     </div>
   );
