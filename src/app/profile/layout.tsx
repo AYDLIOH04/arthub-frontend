2fd737b67eec881f2cc210fc3cbef0bf95c5d1eb
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-01-09 00:45:38 +0500

    фикс багов

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <>
       <PrivatePageProvider>
         <div className="max-w-[1200px] mt-[100px] md:mx-auto mx-2 lg:min-h-[79.6vh] md:min-h-[71.8vh] min-h-[68.7vh]">
-          <ProfileNavbar />
+          {/* <ProfileNavbar /> */}
           {children}
         </div>
         <MainFooter />
