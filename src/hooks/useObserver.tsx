321bfe23cef2fbafd59589af46acbac721b7c9f4
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-01-09 05:11:56 +0500

    рефакторинг

--- src/hooks/useObserver.tsx
+++ src/hooks/useObserver.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
-const useObserver = (cb: any) => {
+const useObserver = (callback: any) => {
   const [isLoading, setIsLoading] = useState(false);
   const [ref, inView] = useInView();
 
   useEffect(() => {
     if (inView && !isLoading) {
       setIsLoading(true);
-      cb();
+      callback();
       setTimeout(() => setIsLoading(false), 200);
     }
-  }, [inView, cb]);
+  }, [inView, callback]);
 
   return ref;
 };
